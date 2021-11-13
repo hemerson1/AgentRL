48c13074d6b361e8f0c3f86b70f03fe20548b751
hemerson1 <emersonharry8@gmail.com>
2021-11-13 12:56:10 +0000

    Added base buffer

---
+++ AgentRL/common/Buffers/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 13 12:15:01 2021
+
+@author: hemerson
+"""
+
+class base_buffer():
+    
+    def __init__(self):
+        
+        # TODO: fill in the structure of the inheritance class
+        pass
+
+    def push(self):
+        raise NotImplementedError        
+        
+    def sample(self):
+        raise NotImplementedError
+        
+        
+if __name__ == '__main__':
+    
+    pass
